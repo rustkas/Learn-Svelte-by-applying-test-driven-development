42cc2f120e8c2757f852639e31c31cb15475a37b
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-15 21:46:31 +0700

    Refactor
    Using bind:value

--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -12,7 +12,7 @@
 <label for="e-mail">E-mail</label>
 <input id="e-mail"/>
 <label for="password">Password</label>
-<input id="password" type="password" on:input={(event) => password = event.target.value}/>
+<input id="password" type="password" bind:value={password}/>
 <label for="password-repeat">Password Repeat</label>
-<input id="password-repeat" type="password" on:input={(event) => passwordRepeat = event.target.value}/>
+<input id="password-repeat" type="password" bind:value={passwordRepeat}/>
 <button {disabled}>Sign Up</button>
