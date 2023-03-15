c529fc51d55aa8f9dba0b3c0d2984669410d9fcc
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-15 21:40:03 +0700

    'Enables the button when the password and password repeat have the same value

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,28 +1,29 @@
-import {render, screen} from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import * as SignUpPage from './SignUpPage.svelte';
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 describe("Sign Up Page", () => {
   describe("Layout", () => {
-    it('has Sign Up header', ()=>{
+    it('has Sign Up header', () => {
       render(SignUpPage);
-      const header = screen.getByRole('heading', {name: 'Sign Up'});
+      const header = screen.getByRole('heading', { name: 'Sign Up' });
       expect(header).toBeInTheDocument();
     });
 
-    it('has user input', ()=>{
+    it('has user input', () => {
       render(SignUpPage);
       const input = screen.getByLabelText('Username');
       expect(input).toBeInTheDocument();
     });
 
-    it('has e-mail input', ()=>{
+    it('has e-mail input', () => {
       render(SignUpPage);
       const input = screen.getByLabelText('E-mail');
       expect(input).toBeInTheDocument();
     });
 
-    it('has password input', ()=>{
+    it('has password input', () => {
       render(SignUpPage);
       const input = screen.getByLabelText('Password');
       expect(input).toBeInTheDocument();
@@ -33,7 +34,7 @@ describe("Sign Up Page", () => {
       expect(passwordInput.type).toBe("password");
     });
 
-    it('has password repeat input', ()=>{
+    it('has password repeat input', () => {
       render(SignUpPage);
       const input = screen.getByLabelText('Password Repeat');
       expect(input).toBeInTheDocument();
@@ -45,16 +46,31 @@ describe("Sign Up Page", () => {
       expect(passwordInput.type).toBe("password");
     });
 
-    it('has Sign Up button', ()=>{
+    it('has Sign Up button', () => {
       render(SignUpPage);
-      const button = screen.getByRole('button', {name: 'Sign Up'});
+      const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).toBeInTheDocument();
     });
-    
-    it('disables the button initially', ()=>{
+
+    it('disables the button initially', () => {
       render(SignUpPage);
-      const button = screen.getByRole('button', {name: 'Sign Up'});
+      const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).toBeDisabled();
     });
   });
+
+  describe("Interactions", () => {
+    it('Enables the button when the password and password repeat have the same value', async () => {
+      render(SignUpPage);
+      const passwordInput = screen.getByLabelText('Password');
+      const passwordRepeatInput = screen.getByLabelText('Password Repeat');
+      await userEvent.type(passwordInput, 'P4ssword');
+      await userEvent.type(passwordRepeatInput, 'P4ssword');
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).not.toBeDisabled();
+      expect(button).toBeEnabled();
+    });
+  });
+
+
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,10 +1,25 @@
+<script>
+  let disabled = true;
+  let password;
+  let passwordRepeat;
+  $: disabled = password !== passwordRepeat;
+
+  const onChangePassword = (event) => {
+    password = event.target.value;
+  }
+
+  const onChangePasswordRepeat = (event) => {
+    passwordRepeat = event.target.value;
+  }
+</script>
+
 <h1>Sign Up</h1>
 <label for="username">Username</label>
 <input id="username"/>
 <label for="e-mail">E-mail</label>
 <input id="e-mail"/>
 <label for="password">Password</label>
-<input id="password" type="password"/>
+<input id="password" type="password" on:input={onChangePassword}/>
 <label for="password-repeat">Password Repeat</label>
-<input id="password-repeat" type="password"/>
-<button disabled>Sign Up</button>
+<input id="password-repeat" type="password" on:input={onChangePasswordRepeat}/>
+<button {disabled}>Sign Up</button>
