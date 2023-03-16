9f3e2b941d65197307c934310bbb0ca112023f23
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-16 09:02:42 +0700

    Sends username, email and password to backend after clicking the button

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/svelte';
 import * as SignUpPage from './SignUpPage.svelte';
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import axios from 'axios';
 
 describe("Sign Up Page", () => {
   describe("Layout", () => {
@@ -69,6 +70,35 @@ describe("Sign Up Page", () => {
       const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).not.toBeDisabled();
       expect(button).toBeEnabled();
+    });
+  
+    it('Sends username, email and password to backend after clicking the button', async () => {
+      render(SignUpPage);
+      const usernameInput = screen.getByLabelText('Username');
+      const emailInput = screen.getByLabelText('E-mail');
+      const passwordInput = screen.getByLabelText('Password');
+      const passwordRepeatInput = screen.getByLabelText('Password Repeat');
+      await userEvent.type(usernameInput, 'user1');
+      await userEvent.type(emailInput, 'user1@mail.com');
+      await userEvent.type(passwordInput, 'P4ssword');
+      await userEvent.type(passwordRepeatInput, 'P4ssword');
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).not.toBeDisabled();
+      
+      const mockFn = jest.fn();
+
+      axios.post = mockFn;
+
+      await userEvent.click(button);
+      
+      const firstCall = mockFn.mock.calls[0];;
+      const body = firstCall[1];
+      expect(body).toEqual({
+        username:'user1',
+        email:'user1@mail.com',
+        password: 'P4ssword'
+      });
+
     });
   });
 
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,18 +1,27 @@
 <script>
+  import axios from 'axios';
+
   let disabled = true;
+  let username;
+  let email;
   let password;
   let passwordRepeat;
   $: disabled = (password && passwordRepeat) ? password !== passwordRepeat : true;
 
+  const submit = () => {
+        axios.post('/api/1.0/users',{
+          username, email, password
+        });
+  };
 </script>
 
 <h1>Sign Up</h1>
 <label for="username">Username</label>
-<input id="username"/>
+<input id="username" bind:value={username}/>
 <label for="e-mail">E-mail</label>
-<input id="e-mail"/>
+<input id="e-mail" bind:value={email}/>
 <label for="password">Password</label>
 <input id="password" type="password" bind:value={password}/>
 <label for="password-repeat">Password Repeat</label>
 <input id="password-repeat" type="password" bind:value={passwordRepeat}/>
-<button {disabled}>Sign Up</button>
+<button {disabled} on:click={submit}>Sign Up</button>
