ef56f3eaf15c83679d915aeeb1dccb1a5686ddad
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-16 09:12:54 +0700

    Refactor
    Sends username, email and password to backend after clicking the button

--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -9,19 +9,21 @@
   $: disabled = (password && passwordRepeat) ? password !== passwordRepeat : true;
 
   const submit = () => {
+      
         axios.post('/api/1.0/users',{
           username, email, password
         });
   };
 </script>
-
-<h1>Sign Up</h1>
-<label for="username">Username</label>
-<input id="username" bind:value={username}/>
-<label for="e-mail">E-mail</label>
-<input id="e-mail" bind:value={email}/>
-<label for="password">Password</label>
-<input id="password" type="password" bind:value={password}/>
-<label for="password-repeat">Password Repeat</label>
-<input id="password-repeat" type="password" bind:value={passwordRepeat}/>
-<button {disabled} on:click={submit}>Sign Up</button>
+<form>
+  <h1>Sign Up</h1>
+  <label for="username">Username</label>
+  <input id="username" bind:value={username}/>
+  <label for="e-mail">E-mail</label>
+  <input id="e-mail" bind:value={email}/>
+  <label for="password">Password</label>
+  <input id="password" type="password" bind:value={password}/>
+  <label for="password-repeat">Password Repeat</label>
+  <input id="password-repeat" type="password" bind:value={passwordRepeat}/>
+  <button {disabled} on:click|preventDefault={submit}>Sign Up</button>
+</form>
